58d28ac3d46f9e20adc3b308b821a9e017bf8167
Kyle Huston <khuston@gmail.com>
2020-07-31 09:21:01 -0400

    Add build configuration for API and use more readable template config.json files

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,6 @@
 cd /webapps/gardenplace/backend/authService
 nohup ./authService > authService.out 2> authService.err </dev/null & echo $! > auth_pidfile
+
+cd /webapps/gardenplace/backend/api
+yarn install
+nohup yarn start > api.out 2> api.err </dev/null & echo $! > api_pidfile
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,2 +1,5 @@
 cd /webapps/gardenplace/backend/authService
 kill $(cat auth_pidfile)
+
+cd /webapps/gardenplace/backend/api
+kill $(cat api_pidfile)
