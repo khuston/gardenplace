cca3735b16295e8a5af83e1a254c3d6edfbe2f74
Kyle Huston <khuston@gmail.com>
2020-07-31 11:39:51 -0400

    Don't rebuild api after deployment

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@ nohup ./authService > authService.out 2> authService.err </dev/null & echo $! > 
 
 cd /webapps/gardenplace/backend/api
 yarn install
-nohup yarn start > api.out 2> api.err </dev/null & echo $! > api_pidfile
+nohup node . > api.out 2> api.err </dev/null & echo $! > api_pidfile
